aa92b48df5248440e8d93f1f1a0721349b58057c
JhonShaker <juan.ferrada.1991@gmail.com>
2023-09-10 23:32:35 -0300

    Integracion de archivos plantilla Lens y ruta Root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'gallery/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+ root "gallery#index"
 end
